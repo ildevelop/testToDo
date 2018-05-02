4993e9b5494815f61e58d8cf901f1ba60fcdcf2f
Ilya Radu <raduilya@gmail.com>
2018-05-02 19:58:49 +0300

    add agenda

--- src/App.js
+++ src/App.js
@@ -7,8 +7,26 @@ import InProcess from "./Components/InProcess/InProcess";
 import ToDos from "./Components/ToDos/ToDos";
 import Button from 'material-ui/Button';
 import AddIcon from '@material-ui/icons/Add';
+import Popover from 'material-ui/Popover';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
 
 class App extends Component {
+
+  state = {
+    open: false,
+  };
+  handleClickButton = () => {
+    this.props.addAgenda;
+    this.setState({
+      open: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
   render() {
     return (
       <div className="App">
@@ -17,10 +35,31 @@ class App extends Component {
           <h1 className="App-title">Welcome to TO-DO list</h1>
         </header>
         <p>open agendas: {this.props.agenda}</p>
-        <Button  onClick={this.props.addAgenda} variant="raised" color="primary">
+        <Button  onClick={this.handleClickButton} variant="raised" color="primary">
           Add Agenda
           <AddIcon />
         </Button>
+        <Popover
+          open={this.state.open}
+          onClose={this.handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+        >
+          <Paper  elevation={4}>
+            <Typography variant="headline" component="h3">
+              This is a sheet of paper.
+            </Typography>
+            <Typography component="p">
+              Paper can be used to build surface or other elements for your application.
+            </Typography>
+          </Paper>
+        </Popover>
         <div className="mainPage">
           <ToDos/>
           <InProcess/>
